10d221930680fecc822afcf259bc42a0d7029834
Daniel Surface <danielsreynolds@ymail.com>
2019-08-07 13:31:06 -0500

    testing pages

---
+++ precache-manifest.1f912bc94be082e55f01bfa39bbb1c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f27fff3081e383ba654b86242b6b680",
+    "url": "/testdeploy/index.html"
+  },
+  {
+    "revision": "9a07f82ab4260007f227",
+    "url": "/testdeploy/static/css/main.d174bc11.chunk.css"
+  },
+  {
+    "revision": "cc16bc5664e039918e97",
+    "url": "/testdeploy/static/js/2.cc3fa00d.chunk.js"
+  },
+  {
+    "revision": "9a07f82ab4260007f227",
+    "url": "/testdeploy/static/js/main.8253d90e.chunk.js"
+  },
+  {
+    "revision": "ad6ad792628b989e4a31",
+    "url": "/testdeploy/static/js/runtime~main.f4eb86ae.js"
+  }
+]);
